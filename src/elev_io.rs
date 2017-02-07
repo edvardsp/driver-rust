826a09820b58a15fcc7a113bdaf0577861e0866b
edvardsp <edvardsp@github.com>
2017-02-07 11:40:18 +0100

    Written short demo using elev_io. Not tested.

--- src/elev_io.rs
+++ src/elev_io.rs
@@ -9,8 +9,12 @@ pub struct ElevIo {
     io: HwIo,
 }
 
-pub type Floor = usize;
-const N_FLOORS: usize = 4;
+#[derive(Copy, Clone)]
+pub enum Floor {
+    At(usize),
+    Between,
+}
+pub const N_FLOORS: usize = 4;
 const TOP: usize = N_FLOORS - 1;
 const SEC_TOP: usize = N_FLOORS - 2;
 
@@ -53,7 +57,7 @@ impl ElevIo {
     pub fn new() -> io::Result<Self> {
         let elev = ElevIo { io: HwIo::new()? };
         elev.set_all_light(Light::Off)?;
-        elev.set_floor_light(0)?;
+        elev.set_floor_light(Floor::At(0))?;
         Ok(elev)
     }
 
@@ -76,9 +80,9 @@ impl ElevIo {
 
     pub fn set_all_light(&self, mode: Light) -> io::Result<()> {
         for floor in 0..N_FLOORS {
-            if floor != TOP { self.set_button_light(Button::CallUp(floor), mode)?; }
-            if floor != 0   { self.set_button_light(Button::CallDown(floor), mode)?; }
-            self.set_button_light(Button::Internal(floor), mode)?;
+            if floor != TOP { self.set_button_light(Button::CallUp(Floor::At(floor)), mode)?; }
+            if floor != 0   { self.set_button_light(Button::CallDown(Floor::At(floor)), mode)?; }
+            self.set_button_light(Button::Internal(Floor::At(floor)), mode)?;
         }
         self.set_stop_light(mode)?;
         self.set_door_light(mode)?;
@@ -90,9 +94,9 @@ impl ElevIo {
         const CALL_DOWN_ADDR: [usize; 3] = [ 0x300+7, 0x300+5, 0x300+4 ];
         const INTERNAL_ADDR: [usize; 4]  = [ 0x300+13, 0x300+12, 0x300+11, 0x300+10 ];
         let addr = match button {
-            Button::CallUp(floor @ 0...SEC_TOP) => CALL_UP_ADDR[floor],
-            Button::CallDown(floor @ 1...TOP) => CALL_DOWN_ADDR[floor-1],
-            Button::Internal(floor @ 0...TOP) => INTERNAL_ADDR[floor],
+            Button::CallUp(Floor::At(floor @ 0...SEC_TOP)) => CALL_UP_ADDR[floor],
+            Button::CallDown(Floor::At(floor @ 1...TOP)) => CALL_DOWN_ADDR[floor-1],
+            Button::Internal(Floor::At(floor @ 0...TOP)) => INTERNAL_ADDR[floor],
             _ => return Err(io::Error::new(io::ErrorKind::InvalidInput, "given floor is not supported for given button")),
         };
         match mode {
@@ -105,14 +109,18 @@ impl ElevIo {
 
     pub fn set_floor_light(&self, floor: Floor) -> io::Result<()> {
         const FLOOR_LIGHT_ADDR: [usize; 2] = [ 0x300+0, 0x300+1 ];
-        if floor > TOP {
-            return Err(io::Error::new(io::ErrorKind::InvalidInput, "given floor is not supported"));
+        if let Floor::At(etg) = floor {
+            if etg > TOP {
+                return Err(io::Error::new(io::ErrorKind::InvalidInput, "given floor is not supported"));
+            }
+            if etg & 0x2 != 0 { self.io.set_bit(FLOOR_LIGHT_ADDR[0])?; } 
+            else              { self.io.clear_bit(FLOOR_LIGHT_ADDR[0])?; }
+            if etg & 0x1 != 0 { self.io.set_bit(FLOOR_LIGHT_ADDR[1])?; }
+            else              { self.io.clear_bit(FLOOR_LIGHT_ADDR[1])?; }
+            Ok(())
+        } else {
+            Err(io::Error::new(io::ErrorKind::InvalidInput, "Cannot set light between floors"))
         }
-        if floor & 0x2 != 0 { self.io.set_bit(FLOOR_LIGHT_ADDR[0])?; } 
-        else                { self.io.clear_bit(FLOOR_LIGHT_ADDR[0])?; }
-        if floor & 0x1 != 0 { self.io.set_bit(FLOOR_LIGHT_ADDR[1])?; }
-        else                { self.io.clear_bit(FLOOR_LIGHT_ADDR[1])?; }
-        Ok(())
     }
 
     pub fn set_door_light(&self, mode: Light) -> io::Result<()> {
@@ -138,25 +146,23 @@ impl ElevIo {
         const CALL_DOWN_ADDR: [usize; 3] = [ 0x200+0, 0x200+2, 0x200+3 ];
         const INTERNAL_ADDR: [usize; 4] = [ 0x300+21, 0x300+20, 0x300+19, 0x300+18 ];
         let addr = match button {
-            Button::CallUp(floor @ 0...SEC_TOP) => CALL_UP_ADDR[floor],
-            Button::CallDown(floor @ 1...TOP) => CALL_DOWN_ADDR[floor-1],
-            Button::Internal(floor @ 0...TOP) => INTERNAL_ADDR[floor],
+            Button::CallUp(Floor::At(floor @ 0...SEC_TOP)) => CALL_UP_ADDR[floor],
+            Button::CallDown(Floor::At(floor @ 1...TOP)) => CALL_DOWN_ADDR[floor-1],
+            Button::Internal(Floor::At(floor @ 0...TOP)) => INTERNAL_ADDR[floor],
             _ => return Err(io::Error::new(io::ErrorKind::InvalidInput, "given floor is not supported for given button")),
         };
         let value = self.io.read_bit(addr)?;
         Ok(Signal::new(value))
     }
 
-    pub fn get_floor_signal(&self, floor: Floor) -> io::Result<Signal> {
+    pub fn get_floor_signal(&self) -> io::Result<Floor> {
         const FLOOR_SENSOR_ADDR: [usize; 4] = [ 0x200+4, 0x200+5, 0x200+6, 0x200+7 ];
-        if floor > TOP {
+        for floor in 0..FLOOR_SENSOR_ADDR.len() {
+            if self.io.read_bit(FLOOR_SENSOR_ADDR[floor])? != 0 {
+                return Ok(Floor::At(floor));
+            }
         }
-        let value = match floor {
-            0...TOP => self.io.read_bit(FLOOR_SENSOR_ADDR[floor])?,
-            _ => return Err(io::Error::new(io::ErrorKind::InvalidInput, "given floor is not supported")),
-        };
-        Ok(Signal::new(value))
-        
+        Ok(Floor::Between) 
     }
 
     pub fn get_stop_signal(&self) -> io::Result<Signal> {
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
 
 extern crate driver;
 
-use driver::elev_io::ElevIo;
+use std::process;
+
+use driver::elev_io::*;
 
 fn main() {
     println!("Hello, world!");
-    if let Err(err) = ElevIo::new() {
-        println!("{}", err);
+    let elev_io: ElevIo = match ElevIo::new() {
+        Ok(elev) => elev,
+        Err(err) => {
+            println!("Init of HW failed with Error: {}", err);
+            process::exit(1);
+        },
+    };
+    
+    elev_io.set_motor_dir(MotorDir::Up).unwrap();
+
+    const SEC_TOP: usize = N_FLOORS - 1;
+    loop {
+        match elev_io.get_floor_signal().unwrap() {
+            Floor::At(SEC_TOP) => elev_io.set_motor_dir(MotorDir::Up).unwrap(),
+            Floor::At(0) => elev_io.set_motor_dir(MotorDir::Down).unwrap(),
+            _ => {}
+        }
+
+        match elev_io.get_stop_signal().unwrap() {
+            Signal::High => { 
+                elev_io.set_motor_dir(MotorDir::Stop).unwrap();
+                process::exit(0);
+            },
+            _ => {}
+        }
     }
 }
